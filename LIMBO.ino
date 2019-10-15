d159def8d5edd8fe330a6a185fecb82f86654acb
MazayKun <kir99916@gmail.com>
2019-10-15 22:37:44 +0300

    Control keys was reworked

--- LIMBO.ino
+++ LIMBO.ino
@@ -6,16 +6,17 @@ int j_x_analog = A1;
 int j_y_analog = A2;
 bool isLeft;
 bool isRight;
-bool isUp;
+bool isJump;
 bool isDown;
 bool isCtrl;
 bool isEsc;
 bool isEnter;
 
 void setup() {
+  Serial.begin(9600);
   isLeft = false;
   isRight = false;
-  isUp = false;
+  isJump = false;
   isDown = false;
   isCtrl = false;
   isEsc = false;
@@ -27,57 +28,45 @@ void setup() {
 void loop() {
   if(millis() - lastCheck > 10){
     lastCheck = millis();
-    if( (analogRead(j_x_analog) > 450) && (analogRead(j_x_analog) < 650) && (isLeft || isRight)){
+    if( (analogRead(j_x_analog) > 50) && (analogRead(j_x_analog) < 850) && (isLeft || isRight)){
       isLeft = false;
       isRight = false;
       Keyboard.release(KEY_LEFT_ARROW);
       Keyboard.release(KEY_RIGHT_ARROW);
     }
-    if( (analogRead(j_x_analog) < 450) && !isLeft){
+    if( (analogRead(j_x_analog) < 50) && !isLeft){
       isLeft = true;
       Keyboard.press(KEY_LEFT_ARROW);
     }
-    if( (analogRead(j_x_analog) > 650) && !isRight){
+    if( (analogRead(j_x_analog) > 850) && !isRight){
       isRight = true;
       Keyboard.press(KEY_RIGHT_ARROW);
     }
-    if( (analogRead(j_y_analog) > 450) && (analogRead(j_y_analog) < 650) && (isUp || isDown)){
-      isUp = false;
-      isDown = false;
-      Keyboard.release(KEY_UP_ARROW);
-      Keyboard.release(KEY_DOWN_ARROW);
-    }
-    if( (analogRead(j_y_analog) < 450) && !isUp){
-      isUp = true;
-      Keyboard.press(KEY_UP_ARROW);
-    }
-    if( (analogRead(j_y_analog) > 650) && !isDown){
-      isDown = true;
-      Keyboard.press(KEY_DOWN_ARROW);
-    }
-    if(((analogRead(color_button_analog) < 350) || (analogRead(color_button_analog) > 380)) && isCtrl){
+ 
+    if(((analogRead(color_button_analog) < 230) || (analogRead(color_button_analog) > 300)) && isCtrl){
       isCtrl = false;
       Keyboard.release(KEY_LEFT_CTRL);
     }
-    if((analogRead(color_button_analog) > 350) && (analogRead(color_button_analog) < 380) && !isCtrl){
+    if((analogRead(color_button_analog) > 230) && (analogRead(color_button_analog) < 300) && !isCtrl){
       isCtrl = true;
       Keyboard.press(KEY_LEFT_CTRL);
     }
-    if(((analogRead(color_button_analog) < 1000) && isEsc)){
-      isEsc = false;
-      Keyboard.release(KEY_ESC);
+    if(((analogRead(color_button_analog) < 1000) && isJump)){
+      isJump = false;
+      Keyboard.release(KEY_UP_ARROW);
     }
-    if((analogRead(color_button_analog) > 1000) && !isEsc){
-      isEsc = true;
-      Keyboard.press(KEY_ESC);
+    if((analogRead(color_button_analog) > 1000) && !isJump){
+      isJump = true;
+      Keyboard.press(KEY_UP_ARROW);
     }
-    if(((analogRead(color_button_analog) < 300) || (analogRead(color_button_analog) > 315)) && isEnter){
-      isEnter = false;
-      Keyboard.release(KEY_RETURN);
+    if(((analogRead(color_button_analog) < 300) || (analogRead(color_button_analog) > 390)) && isDown){
+      isDown = false;
+      Keyboard.release(KEY_DOWN_ARROW);
     }
-    if((analogRead(color_button_analog) > 300) && (analogRead(color_button_analog) < 315) && !isEnter){
-      isEnter = true;
-      Keyboard.press(KEY_RETURN);
+    if((analogRead(color_button_analog) > 300) && (analogRead(color_button_analog) < 390) && !isDown){
+      isDown = true;
+      Keyboard.press(KEY_DOWN_ARROW);
     }
+    Serial.println(analogRead(j_x_analog));
   }
 }
